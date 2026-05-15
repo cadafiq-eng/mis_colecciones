56df78db1444aed801822c9f5a25052670142c9d
cadafiq-eng <cadafiq@gmail.com>
2026-05-14 18:39:07 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
-// MisColecciones PWA - Service Worker
-const CACHE = 'miscolecciones-v1';
+// MisColecciones PWA - Service Worker v2
+const CACHE = 'miscolecciones-v2';
 const ASSETS = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
+  // Share target: let it through to the page (don't cache share requests)
+  if (e.request.url.includes('title=') || e.request.url.includes('url=')) {
+    return;
+  }
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
   );
